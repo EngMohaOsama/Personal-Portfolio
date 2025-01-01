261ae300d5a935414fd2ac62a9f337a48fe4ce79
Osama <eng13mohamedosama@gmail.com>
2025-01-01 11:15:31 +0200

    Update menu animation and menu for mobil application

--- js/main.js
+++ js/main.js
@@ -50,3 +50,19 @@ getData("https://api.github.com/users/EngMohaOsama/repos")
     .catch((rej) => {
         console.log(rej);
     });
+
+/*=============== SHOW MENU ===============*/
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId)
+
+    toggle.addEventListener('click', () => {
+        // Add show-menu class to nav menu
+        nav.classList.toggle('ShowMenu')
+
+        // Add show-icon to show and hide the menu icon
+        toggle.classList.toggle('ShowIcon')
+    })
+}
+
+showMenu('NavToggle', 'NavMenu')
